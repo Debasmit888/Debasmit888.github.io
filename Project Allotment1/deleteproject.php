6f774207342e942f4202bdf7d96a83f8fe88fa65
Debasmit888 <78145288+Debasmit888@users.noreply.github.com>
2021-12-24 20:35:19 +0530

    Delete Project Allotment1 directory

--- Project Allotment1/deleteproject.php
+++
@@ -1,8 +0,0 @@
-<?php
- require 'init.php';
-if(isset($_GET['id']) AND !empty($_GET['id'])){
-	$id = $_GET['id'];
-
-	$query = $db->query("DELETE FROM project WHERE id = $id ");
-	header('location: create-project.php');
-}
--- Project Allotment1/session.php
+++
@@ -1,4 +0,0 @@
-<?php
-if(!$_SESSION['id'] or empty($_SESSION['id']) or $_SESSION['id'] == ''){
-	header('location: index.php');
-}
